09c0090a726b2211ccdc0e2268c18c51f1820183
AshutoshRavindraIwale <122389422+AshutoshRavindraIwale@users.noreply.github.com>
2023-01-20 17:54:28 -0500

    Delete LAB0 directory

--- LAB0/catkin_ws/build/rospy/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ashutosh/catkin_ws/src/rospy
-# Build directory: /home/ashutosh/catkin_ws/build/rospy
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
